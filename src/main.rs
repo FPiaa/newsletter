e0558a8bbce84571575cf62bc43bd512342ebb0e
Igor Fidler <igorgfidler@hotmail.com>
2023-07-29 18:41:36 -0300

    teste

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
 
 use newsletter::app_server;
+use newsletter::configuration;
 
 #[tokio::main]
 async fn main() -> hyper::Result<()> {
@@ -8,6 +9,11 @@ async fn main() -> hyper::Result<()> {
         .with_max_level(tracing::Level::DEBUG)
         .init();
 
-    let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
+    let configuration =
+        configuration::get_configuration().expect("Unable to read configuration file");
+
+    let address = format!("127.0.0.1:{}", configuration.database.port);
+
+    let listener = TcpListener::bind(address).unwrap();
     app_server(listener).await
 }
--- tests/subscriber_endpoint.rs
+++ tests/subscriber_endpoint.rs
@@ -1,12 +1,21 @@
 use axum::http::StatusCode;
+use newsletter::configuration::get_configuration;
+use sqlx::{Connection, PgConnection};
 mod common;
 
 #[tokio::test]
 async fn subscribe_returns_200_when_valid_form_data() {
     let app_address = common::spawn_app();
+    let configuration = get_configuration().expect("Unable to read configuration file");
+
+    let connection_string = configuration.database.connection_string();
     let client = reqwest::Client::new();
 
     let body = "name=1e%20guin&email=ursula_le_gin%40gmail.com";
+
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("failed to connect to postgres");
 
     let response = client
         .post(&format!("{app_address}/subscription"))
@@ -16,7 +25,14 @@ async fn subscribe_returns_200_when_valid_form_data() {
         .await
         .expect("Failed to execute request in subscribe returns 200");
 
+    let saved_user = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription");
+
     assert_eq!(StatusCode::OK, response.status().as_u16());
+    assert_eq!(saved_user.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved_user.name, "le guin");
 }
 
 #[tokio::test]
